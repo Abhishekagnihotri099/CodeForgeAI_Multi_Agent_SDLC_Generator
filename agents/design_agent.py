9e941c96821dc184c0ebd8ddd05be2b9b4592bd6
Abhishek Agnihotri <89059338+Abhishekagnihotri099@users.noreply.github.com>
2026-01-05 13:08:05 +0530

    Update design_agent.py

--- agents/design_agent.py
+++ agents/design_agent.py
@@ -1,4 +1,4 @@
-from autogen.agentchat import AssistantAgent
+from autogen_agentchat import AssistantAgent
 import streamlit as st
 
 SYSTEM_MESSAGE = """
@@ -54,4 +54,5 @@ design_agent = AssistantAgent(
         "max_tokens": 2000,
     },
 )
+
 
